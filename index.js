e21135d127e065d6bc50659febdb56619104cbef
geoffrey woodcock <theatomproject@gmail.com>
2019-04-17 18:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* Your code goes here */
+var customerName = 'bob';
+const leastFavoriteCustomer = "jim"
+// let favoriteCustomer = "jack";
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+   bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "joe";
+}
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "william";
+  let favoriteCustomer = "jenny";
+}
