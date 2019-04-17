2833996bc8a34e2604277f98ed3976a2cea7428c
geoffrey woodcock <theatomproject@gmail.com>
2019-04-17 18:31:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function changeLeastFavoriteCustomer(){
 }
 function attemptTwoFavoriteCustomers(){
   let favoriteCustomer = "william";
-  let favoriteCustomer = "joe";
+  // let favoriteCustomer = "joe";
 }
